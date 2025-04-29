0072272949128ba8579ea01ec34b2289f3e3e6e3
pawan <spavandsp@gmail.com>
2025-04-30 00:13:21 +0530

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routes/index.js";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+dotenv.config();
+app.use(express.json());
+app.use("/api", router);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
